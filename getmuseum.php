f9db861843759c55057de4ae46eeea6b9dd9406c
Asad Khan <akhan625@uwo.ca>
2019-11-12 18:10:46 +0000

    message

---
+++ getmuseum.php
@@ -0,0 +1,11 @@
+<?php
+   $query = "SELECT * FROM museum";
+   $result = mysqli_query($connection, $query);
+   if (!$result) {
+	die("databases query failed. ");
+}
+while ($row = mysqli_fetch_assoc($result)) {
+	echo "<option> $row["musname"] </option>"
+	}
+	mysqli_free_result($result);
+?>
